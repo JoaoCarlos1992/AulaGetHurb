0d9856e3e03117da7c264856339b460b8ef63370
junior.joao <c.junior@pucpr.edu.br>
2023-09-18 11:01:10 -0300

    Redatorando o main

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,5 +1,9 @@
 public class Exemplo {
+    private static void sayHello() {
+        System.out.println("Olá mundo!");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        sayHello();
     }
 }
